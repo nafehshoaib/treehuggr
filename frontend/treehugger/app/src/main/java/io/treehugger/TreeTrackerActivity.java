6b48381eb781417310e01e4381cceab30e5839a3
isaiah <isaiahballah@gmail.com>
2018-11-03 11:22:51 -0700

    Added Register Device activties and tree tracker activities

---
+++ frontend/treehugger/app/src/main/java/io/treehugger/TreeTrackerActivity.java
@@ -0,0 +1,13 @@
+package io.treehugger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TreeTrackerActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tree_tracker);
+	}
+}
